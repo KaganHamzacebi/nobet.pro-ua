c9b6dec5bfef8f8d911dfd2e726dd944ff70468b
KaganHamzacebi <kagan.hcebi@gmail.com>
2024-05-04 02:20:21 +0300

    sonar fixes & start-database.sh removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: Readonly<React.ReactNode>;
 }) {
   return (
     <html lang="en">
