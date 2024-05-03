e6fec87bd546410c1fa2626903f83b0fe5a2de2d
KaganHamzacebi <kagan.hcebi@gmail.com>
2024-05-04 01:37:18 +0300

    prisma accelerator added

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,12 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
 import { env } from "@/env";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const createPrismaClient = () =>
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+const prisma = new PrismaClient().$extends(withAccelerate());
+
+const createPrismaClient = () => prisma;
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
