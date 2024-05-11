e895895f5774f1f68825bad1393e40764719e971
KaganHamzacebi <kagan.hcebi@gmail.com>
2024-05-11 17:07:58 +0300

    eslint configuration fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
+  ignorePatterns: ["node_modules/", "src/components/icons"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
