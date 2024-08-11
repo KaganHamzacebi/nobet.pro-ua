93839b2c37da6009ba98b8f5aa72db05fd33770d
KaganHamzacebi <kagan.hcebi@gmail.com>
2024-08-11 22:40:15 +0300

    added new configs

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+const config = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: 'true',
+  singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss']
+};
+
+export default config;
