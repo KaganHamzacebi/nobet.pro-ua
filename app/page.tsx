c2ee8e30eb6fab6d0e0d883afd3c708e87de49ab
KaganHamzacebi <kagan.hcebi@gmail.com>
2024-10-03 00:13:36 +0300

    suspense added to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Login from '@/components/ui/login';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <div className="mt-20 flex h-full w-full items-center justify-center">
-      <Login />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Login />
+      </Suspense>
     </div>
   );
 }
